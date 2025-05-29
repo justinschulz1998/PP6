c6b3cbd007b986bc95480f46322c36b0ab5d25b5
justin schulz <justinschulz2014hotmail.de>
2025-05-29 21:59:45 +0200

    Aufgabe 3

---
+++ solutions/print.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+
+int main(void) {
+	    printf("Hello from C!\n");
+	        printf("Integer: %d, Float: %.2f, String: %s\n", 42, 3.14, "test");
+		    puts("This is puts output");
+		        return 0;
+}
