4bd48c41330539cc2b45544ee58943800ad9d194
justin schulz <justinschulz2014hotmail.de>
2025-05-29 22:34:31 +0200

    Aufgabe 4

---
+++ solutions/print.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+def main():
+            print("Hello from Python3!")
+            print("A", "B", "C", sep="-", end=".\n")
+            value = 2 + 2
+            print(f"Two plus two equals {value}")
+
+if __name__ == "__main__":
+            main()
+
